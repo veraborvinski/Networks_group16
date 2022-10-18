8ef53798474aef2b7c59c38a0c82f14dee9f36a4
veraborvinski <72395680+veraborvinski@users.noreply.github.com>
2022-10-18 13:20:12 +0100

    Update server2.py

--- bot/server2.py
+++ bot/server2.py
@@ -10,15 +10,15 @@ import types
 
 sel = selectors.DefaultSelector()
 
+irc = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+
 #defining global variables
 host = "fc00:1337::17"
 port = 6667
-usernr = 0
-userList = list(range(100))
 
 #class used to hold user objects
 class User:
-	def __init__(self, n):
+	def __init__(self, n, s = "server", soc = irc):
 		self.name = n
 	
 class Channel:
@@ -30,30 +30,44 @@ class Server:
 	def __init__(self, n, ul = set()):
 		self.name = n
 		self.userList = ul
-   
-irc = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-
-#try to bind socket to host and port
-#control for errors: https://medium.com/python-pandemonium/python-socket-communication-e10b39225a4c
-try:
-	irc.bind((host, port))
-except socket.error as err:
-	print(err)
-	sys.exit(1)
+		
+	def start(self):
+		#try to bind socket to host and port
+		#control for errors: https://medium.com/python-pandemonium/python-socket-communication-e10b39225a4c
+		try:
+			irc.bind((host, port))
+		except socket.error as err:
+			print(err)
+			sys.exit(1)
 	 
-#listen to whether a client connects   
-irc.listen(1)
+		#listen to whether a client connects   
+		irc.listen(1)
 
-irc.setblocking(False)
-sel.register(irc, selectors.EVENT_READ, data=None)
+		irc.setblocking(False)
+		sel.register(irc, selectors.EVENT_READ, data=None)
+		print('\nWaiting for a connection')
+
+		try:
+			#while a connection is not recieved, let user know we are waiing for a connection
+			while True:
+				events = sel.select(timeout=None)
+				for key, mask in events:
+					if key.data is None:
+						accept_wrapper(key.fileobj)
+					else: 
+						service_connection(key, mask)
+		except KeyboardInterrupt:
+			print(error)
+		finally:
+			sel.close()
+   
 
 def accept_wrapper(sock):
 	conn, addr = sock.accept()  # Should be ready to read
 	print(f"Accepted connection from {addr}")
 	conn.setblocking(False)
 	data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
-	events = selectors.EVENT_READ | selectors.EVENT_WRITE
-	sel.register(conn, events, data=data)
+	sel.register(conn, events.EVENT_READ, data=data)
 
 def service_connection(key, mask):
 	sock = key.fileobj
@@ -66,12 +80,6 @@ def service_connection(key, mask):
 			print(f"Closing connection to {data.addr}")
 			sel.unregister(sock)
 			sock.close()
-	if mask & selectors.EVENT_WRITE:
-		if data.outb:
-			msg = (sock.recv(2048).decode("UTF-8")).strip('nr')
-			process_msg(msg)
-			sent = sock.send(data.outb)  # Should be ready to write
-			data.outb = data.outb[sent:]
 
 def process_msg(msg):
 	print(msg)
@@ -87,7 +95,6 @@ def process_msg(msg):
 		print(error)
 
 def process_nick(arg):
-	server = Server("server")
 	user = User(arg)
 	server.userList.add(user)
 	print(user.name)
@@ -97,31 +104,10 @@ def process_user(arg):
 
 def process_join(arg):
 	channel = Channel(arg) 
-			
-print('\nWaiting for a connection')
-
-try:
-	#while a connection is not recieved, let user know we are waiing for a connection
-	while True:
-		events = sel.select(timeout=None)
-		for key, mask in events:
-			if key.data is None:
-				accept_wrapper(key.fileobj)
-			else: 
-				service_connection(key, mask)
-except KeyboardInterrupt:
-	print(error)
-finally:
-	sel.close()
 	
 #the main method runs as long as the server is running
 def main():
-	#keeping server alive using infinite loop and receiving messages: https://acloudguru.com/blog/engineering/creating-an-irc-bot-with-python3
-	while True:
-		#how to decode messages: https://acloudguru.com/blog/engineering/creating-an-irc-bot-with-python3
-		msg = (connection.recv(2048).decode("UTF-8")).strip('nr')
-		print("hi")
-		process_msg(msg)
+	server = Server("server")
         
 main()
    
